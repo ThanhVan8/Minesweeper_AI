5d3431424f4eb17576f094c926f7e33796bd7b43
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-08-11 10:04:51 +0700

    check cell if have info

--- from pysat.py
+++ from pysat.py
@@ -3,14 +3,71 @@ from pysat.solvers import Solver
 import numpy as np
 cnf = CNF()
 
-
-mine = [['1','1','1'],
-        ['-','-','-'],
-        ['-','2','-']]
+# mine = [['1','1','1'],
+#         ['-','-','-'],
+#         ['-','2','-']]
 
 # mine = [['3','-','-'],
 #         ['-','-','-'],
 #         ['-','-','-']]
+
+# mine = [['0','-','-'],
+#         ['1','-','1'],
+#         ['1','-','-']]
+
+# mine = [['-','0','-','0','-','2','-','3','-'],
+#         ['0','-','0','-','-','2','-','5','-'],
+#         ['-','0','-','-','-','1','2','-','-'],
+#         ['0','-','-','-','0','-','1','3','3'],
+#         ['-','1','1','1','-','-','-','1','-'],
+#         ['0','1','-','1','-','0','-','1','1'],
+#         ['0','1','1','1','-','-','-','-','-'],
+#         ['2','2','1','-','0','-','1','1','1'],
+#         ['-','-','1','-','0','-','1','-','1']]
+
+# mine = [[' ','0',' ','0',' ','2','x','3','-'],
+#         ['0',' ','0',' ',' ','2','x','5','-'],
+#         [' ','0',' ',' ',' ','1','2','x','x'],
+#         ['0',' ',' ',' ','0',' ','1','3','3'],
+#         [' ','1','1','1',' ',' ',' ','1','x'],
+#         ['0','1','x','1',' ','0',' ','1','1'],
+#         ['0','1','1','1',' ',' ',' ',' ',' '],
+#         ['2','2','1',' ','0',' ','1','1','1'],
+#         ['x','x','1',' ','0',' ','1','x','1']]
+
+# mine = [['0','0','0','0','0','2','-','3','-'],
+#         ['0','0','0','0','0','2','-','5','3'],
+#         ['0','0','0','0','0','1','2','-','-'],
+#         ['0','0','0','0','-','-','1','3','3'],
+#         ['0','1','1','1','-','-','-','1','-'],
+#         ['-','1','-','1','-','0','-','1','1'],
+#         ['-','1','1','1','-','-','-','-','-'],
+#         ['2','2','1','-','-','-','1','1','1'],
+#         ['-','-','1','-','0','-','1','-','1']]
+
+mine = [['-','1','-','1','-'],
+        ['-','2','1','-','-'],
+        ['3','-','-','2','2'],
+        ['2','-','-','1','-'],
+        ['-','1','1','1','1']]
+
+# mine = [[' ','1',' ','1',' '],
+#         ['x','2','1',' ','x'],
+#         ['3','x',' ','2','2'],
+#         ['2','x',' ','1','x'],
+#         [' ','1','1','1','1']]
+
+# mine = [['-','-','-','1','-'],
+#         ['-','2','1','-','-'],
+#         ['3','-','-','-','2'],
+#         ['-','-','-','1','-'],
+#         ['-','1','1','1','1']]
+
+# mine = [['-','-','-','1','-'],
+#         ['x','2','1','x','-'],
+#         ['3','-','-','-','2'],
+#         ['-','-','-','1','-'],
+#         ['-','1','1','1','1']]
 
 mine=np.array(mine)
 
@@ -61,12 +118,16 @@ def CreateCNF(InitMatrix, cnf):
         for j in range(len(InitMatrix[i])):
             if  InitMatrix[i][j].isnumeric():
                 neighbor_list = neighbors(InitMatrix, (i, j))
-
-                #positive num (having bomb)
-                pos = combinations_positive(neighbor_list,len(neighbor_list)-int(InitMatrix[i][j]) + 1)
-                #negative num (not having bomb)
-                if len(neighbor_list) - int(InitMatrix[i][j]) != 1:
-                    neg = combinations_negative(neighbor_list,len(neighbor_list) - int(InitMatrix[i][j]))
+                if int(InitMatrix[i][j]) == 0:
+                    neg = combinations_negative(neighbor_list,1)
+                elif int(InitMatrix[i][j]) == len(neighbor_list):
+                    pos = combinations_positive(neighbor_list,1)
+                else:
+                    #positive num (having bomb)
+                    pos = combinations_positive(neighbor_list,len(neighbor_list)-int(InitMatrix[i][j]) + 1)
+                    #negative num (not having bomb)
+                    if len(neighbor_list) - int(InitMatrix[i][j]) != 1:
+                        neg = combinations_negative(neighbor_list,len(neighbor_list) - int(InitMatrix[i][j]))
                 
                 for clause in pos:
                     clause = [int(literal) for literal in clause]
@@ -84,13 +145,9 @@ def CreateCNF(InitMatrix, cnf):
         cnf.clauses.remove([])  
     return cnf     
 
-
-    
 CreateCNF(mine, cnf)       
 for clause in cnf.clauses:
     print(clause)
-    
-
 
 with Solver(bootstrap_with=cnf) as solver:
     # 1.1 call the solver for this formula:
@@ -98,3 +155,55 @@ with Solver(bootstrap_with=cnf) as solver:
 
     # 1.2 the formula is satisfiable and so has a model:
     print('and the model is:', solver.get_model())
+
+# def Astar(mine, cnf):
+
+def resolve(c1, c2):
+    clause = []
+    n = min(len(c1), len(c2))
+    [tmp1, tmp2] = [c1, c2] if n == len(c1) else [c2, c1]
+    for v1 in tmp1:
+        for v2 in tmp2:
+            if v1 == -v2:
+                tmp1.remove(v1)
+                tmp2.remove(v2)
+                tmp1.extend(tmp2)
+                clause.extend(list(set(tmp1)))
+                break
+    for i in range(len(clause)-1):
+            if -clause[i] in clause:
+                return []
+    return clause
+
+def resolution(cnf):
+    tmpClause = cnf.clauses.copy()
+    singleVars = [tmp[0] for tmp in tmpClause if len(tmp) == 1]
+
+    for i in range(len(tmpClause)-1):
+        for j in range(i+1, len(tmpClause)):
+            resolvents = resolve(tmpClause[i].copy(), tmpClause[j].copy())
+            if len(resolvents)!=0:
+                tmpClause.append(resolvents)
+            if len(resolvents) == 1:
+                if resolvents[0] not in singleVars:
+                    singleVars.append(resolvents[0])
+                    if -resolvents[0] in singleVars:
+                        return []
+    
+    return singleVars
+
+def singleVars(cnf):
+    return [tmp[0] for tmp in cnf.clauses if len(tmp) == 1]
+
+def checkIfHaveInfo(puzzle, cell):
+    if puzzle[cell[0]][cell[1]] == 0:
+        adjPoint = [-1,0,1]
+        for i in adjPoint:
+            for j in adjPoint:
+                if i == 0 and j == 0:
+                    continue
+                if 0 <= cell[0]+i < len(puzzle) and 0 <= cell[1]+j < len(puzzle[0]):
+                    if puzzle[cell[0]+i][cell[1]+j] != '0':
+                        return False
+        return True
+    return False
--- test.py
+++ test.py
@@ -1,2 +1,46 @@
-a = ['1', '2', '3']
-print(a[1:])
+from pysat.formula import CNF
+from pysat.solvers import Solver
+import numpy as np
+# a = [4, 5]
+# b = [-4, 5, -6]
+# a.extend(b)
+# print(a)
+# print(list(set(a)))
+
+def resolve(c1, c2):
+    clause = []
+    n = min(len(c1), len(c2))
+    [tmp1, tmp2] = [c1, c2] if n == len(c1) else [c2, c1]
+    for v1 in tmp1:
+        for v2 in tmp2:
+            if v1 == -v2:
+                tmp1.remove(v1)
+                tmp2.remove(v2)
+                tmp1.extend(tmp2)
+                clause.extend(list(set(tmp1)))
+                break
+    for i in range(len(clause)-1):
+            if -clause[i] in clause:
+                return []
+    return clause
+
+def resolution(cnf):
+    tmpClause = cnf.clauses.copy()
+    singleVars = [tmp[0] for tmp in tmpClause if len(tmp) == 1]
+
+    for i in range(len(tmpClause)-1):
+        for j in range(i+1, len(tmpClause)):
+            resolvents = resolve(tmpClause[i].copy(), tmpClause[j].copy())
+            if len(resolvents)!=0:
+                tmpClause.append(resolvents)
+            if len(resolvents) == 1:
+                if resolvents[0] not in singleVars:
+                    singleVars.append(resolvents[0])
+                    if -resolvents[0] in singleVars:
+                        return []
+    
+    return singleVars
+
+
+cnf = CNF(from_clauses=[[4, 5], [4, 5, 6], [-4, -5], [-4, -6], [-5, -6], [5, 6], [4, 5, 6, 7], [4, 5, 6, 9], [4, 5, 7, 9], [4, 6, 7, 9], [5, 6, 7, 9], [-4, -5, -6], [-4, -5, -7], [-4, -5, -9], [-4, -6, -7], [-4, -6, -9], [-4, -7, -9], [-5, -6, -7], [-5, -6, -9], [-5, -7, -9], [-6, -7, -9]])
+print(resolution(cnf))
