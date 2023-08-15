d98e7d81c9db6ca3e901bf3ba8035793f274ba18
PhuocTaii <95843466+PhuocTaii@users.noreply.github.com>
2023-08-15 16:37:25 +0700

    Add files via upload

---
+++ MineSweeper.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    print("1. Bruteforce")
+    print("2. Backtracking")
+    print("3. A*")
+    choice = int(input("Your choice: "))
+    
+    if choice == 1:
+        # do everything in bruteforce
+        print('hello')
+        
+    if choice == 2:
+        print('hello')
+        # do everything in backtracking
+    if choice == 3:
+        # de everything in A*
+        print('hello')
+        
